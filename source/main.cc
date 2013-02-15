3d2c63f789334fd547a32d605a304b8c4255cdd3
Zinon Kyprianou <zen@kyprianou.eu>
2013-02-15 13:34:43 +0000

    Added the OGDF graphing library and also created an example graph in the main.

--- source/main.cc
+++ source/main.cc
@@ -14,8 +14,13 @@ Copyright (c) King's College London
 #include "generator.cc"
 #include "engine.cc"
 #include "sysio.cc"
+#include "mapcreate.cc"
+#include <ogdf/basic/Graph.h>
+#include <ogdf/basic/GraphAttributes.h>
 
 using namespace std;
+using namespace ogdf;
+
 
 int main ()
 {
@@ -24,6 +29,34 @@ int main ()
    pthread_attr_t attr;
    void *status;
 
+Graph G;
+	GraphAttributes GA(G, GraphAttributes::nodeGraphics |	
+		GraphAttributes::edgeGraphics );
+ 
+	const int LEN = 11;
+	for(int i = 1; i<LEN; ++i) {
+		node left = G.newNode();
+		GA.x(left) = -5*(i+1);
+		GA.y(left) = -20*i;		
+		GA.width(left) = 10*(i+1);
+		GA.height(left) = 15;
+ 
+		node bottom = G.newNode();
+		GA.x(bottom) = 20*(LEN-i);		
+		GA.y(bottom) = 5*(LEN+1-i);
+		GA.width(bottom) = 15;
+		GA.height(bottom) = 10*(LEN+1-i);
+ 
+		edge e = G.newEdge(left,bottom);
+		DPolyline &p = GA.bends(e);
+		p.pushBack(DPoint(10,-20*i));
+		p.pushBack(DPoint(20*(LEN-i),-10));
+	}
+ 
+	GA.writeGML("manual_graph.gml");
+ 
+	return 0;
+	
    // Initialize and set thread joinable
    pthread_attr_init(&attr);
    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
