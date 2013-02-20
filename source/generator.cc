f6183202d007b62de5ef5e28abb12e6b511a45b5
Zinon Kyprianou <zen@kyprianou.eu>
2013-02-20 19:57:37 +0000

    Creating vehicles of all types and storing them in an array

--- source/generator.cc
+++ source/generator.cc
@@ -11,6 +11,7 @@ Copyright (c) King's College London
 #include "th_structs.h"
 #include "vehicle.h"
 
+
 using namespace std;
 
 
@@ -21,8 +22,6 @@ void *generator(void *arguments)
    
    cout << "Generator Thread with id : " << "1" << "  is created" << endl;
    srand(time(NULL));
-
-   vehicle **temp_array;
 
   while(!thread_args->gen_finished) {
 
@@ -40,33 +39,35 @@ void *generator(void *arguments)
 	*/
 		int actual_no_to_create = rand() % thread_args->max_no_vehicles + 1;
 		if (actual_no_to_create < 10) actual_no_to_create = 10; //maybe revise
+		vehicle* temp_array[actual_no_to_create];
 	/*
 		STEP 3 DECIDE BASED RATIOS HOW MANY OF EACH VEHICLE TYPE YOU WILL CREATE
 		STEP 4 CREATE THE VEHICLES AND STORE IN AN TEMP ARRAY OF OBJECTS
 	*/
-		temp_array = new vehicle*[actual_no_to_create];
 		for (int z=0; z<actual_no_to_create;z++) temp_array[z] = NULL; //initalize array to null pointers
 
-		for (int i=1; i<=actual_no_to_create;i++) {
-			vehicle *new_vehicle = new vehicle();
+		for (int i=0; i<actual_no_to_create;i++) {
+			temp_array[i] = new vehicle();
 			int type_no = rand() % 100 + 1;
 			cout << "MY TYPE NO IS:" << type_no << endl;
 			if (type_no <= thread_args->vehicle_ratios[0]*100) {
-				new_vehicle->setType(0); // car
+				temp_array[i]->setType(0); // car
 			} else if ((type_no > thread_args->vehicle_ratios[0]*100) && (type_no <= ((thread_args->vehicle_ratios[0]*100) + (thread_args->vehicle_ratios[1]*100))) ) {
-				new_vehicle->setType(1); //bus
+				temp_array[i]->setType(1); //bus
 			} else if (type_no > ((thread_args->vehicle_ratios[0]*100) + (thread_args->vehicle_ratios[1]*100))) {
-				new_vehicle->setType(2);
+				temp_array[i]->setType(2);
 			}
 	/*
 		STEP 5 SAVE IN TEMP ARRAY AND SHUFFLE
 	*/
-			for (int j=0; j<actual_no_to_create; j++) {
-				if (temp_array[j] != NULL) temp_array[j] = new_vehicle;
-			}
+	//		for (int j=0; j<actual_no_to_create; j++) {
+	//			if (temp_array[j] == NULL) temp_array[j] = new_vehicle;
+	//		}		
 		}
 
-
+		for (int k=0; k<actual_no_to_create; k++) {
+			cout <<  temp_array[k]->getType() << endl;
+		}
 	/*
 		
 
@@ -76,7 +77,6 @@ void *generator(void *arguments)
 
   	*/
 	/* STEP 7: WAIT FOR THE NEXT ROUND DEPENDING ON THE SLEEP TIMER */
-		delete[] temp_array;
 		sleep(thread_args->sleep_time);
 
   }
